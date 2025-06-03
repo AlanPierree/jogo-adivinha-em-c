feb3df632e59dd3b5a5f09cc6452e62690326ec8
Alan <161715146+AlanPierree@users.noreply.github.com>
2025-06-03 09:41:07 -0300

    Update and rename programa.c to adivinhacao.c

---
+++ adivinhacao.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    // imprime cabeçalho do nosso jogo
+    printf("******************************************\n");
+    printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+    int numerosecreto = 42;
+
+    int chute;
+    
+    for(int i = 1; i <= 3; i++) {
+        printf("Tentativa %d de 3\n", i);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+    
+        int acertou = (chute == numerosecreto);
+    
+         if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+               // PARAR DE EXECUTAR FOR
+            break;
+        }  else {
+             
+            int maior = (chute > numerosecreto);
+            if(maior) {
+                 printf("Seu chute foi maior que o número secreto\n");
+            } else {
+                 printf("Seu chute foi menor que o número secreto\n");
+            }
+        }
+    }
+
+    printf("Fim de jogo!\n")
+} 
